f51cbef4399a49f4bbc793ccffdbe4fcb442871d
anastasiia92 <nastia.dmytryshyna@gmail.com>
2023-03-18 10:14:16 -0400

    login front end

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
+import User from './components/User';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <User />
     </div>
   );
 }
 
 export default App;
+
+
