7947486398dec4315470dca1553341871d695a0d
Dwayne Jones <dwaynejones13123@gmail.com>
2023-03-21 21:05:10 -0400

    working frontend

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,15 @@
 function Home () {
     return (
         <div className="container">
-            <h1>Home</h1>
+            <h1><i> 💃🏽 Welcome To Melomania 💃🏽</i></h1>
+            
+            <h2> Where you can add your favorite songs to listen to and share it with others!🥳  </h2>
+
+            <h2> 🚨In order to use this webiste you must either login or create an account.🚨</h2>
+
+            
         </div>
+        
     )
 }
 
